512400ba9ac889b11394f037974d5c2c6445beb0
ElenaVereskun <lena.vereskun@yandex.ru>
2024-05-15 19:51:59 +0300

    [+]response

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,57 @@
 import Link from "next/link";
+import Image from "next/image";
+import search from "./images/search.svg";
 import styles from "./styles/index.module.css";
 
 const Index = () => {
+  let arrData = [];
+  console.log(arrData);
+
+  function getData() {
+    var url =
+      "http://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/party";
+    var token = "04d61ad173df85b960cc9e9ed8d948dbecf2d939";
+    var query = "7707083893";
+
+    var options = {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: "Token " + token,
+      },
+      body: JSON.stringify({ query: query }),
+    };
+
+    fetch(url, options)
+      .then((response) => (arrData = response.text()))
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  }
+
   return (
-    <div className={styles.container}>
-      <div>
-        <Link href="/">Main</Link>
-        <Link href="/users">Users</Link>
+    <div>
+      <div className={styles.container}>
+        {/*  <Link href="/">Main</Link>
+        <Link href="/users">Users</Link> */}
+        <div className={styles.search}>
+          <input
+            className={styles.input}
+            value={457}
+            placeholder="Укажите ИНН"
+          ></input>
+          <button onClick={getData}>
+            <Image
+              src={search}
+              className={styles.inputImg}
+              alt="поиск"
+              width={40}
+              height={40}
+            />
+          </button>
+        </div>
       </div>
-      <h1>Main page</h1>
     </div>
   );
 };
--- pages/info/[id].js
+++ pages/info/[id].js
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 
-export default function ({ user }) {
+export default function ({ taxNumber }) {
   const { query } = useRouter();
 
   return (
@@ -14,8 +14,8 @@ export async function getServerSideProps({ params }) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/users/${params.id}`
   );
-  const user = await response.json();
+  const taxNumber = await response.json();
   return {
-    props: { user },
+    props: { taxNumber },
   };
 }
